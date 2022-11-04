75effbae8e34d2f945e0370438447d3dadf6035d
juandelia03 <63162510+juandelia03@users.noreply.github.com>
2022-11-04 16:51:35 -0300

    Update tp.hs

--- tp.hs
+++ tp.hs
@@ -39,8 +39,34 @@ distancia:: Complejo -> Complejo -> Float
 distancia c i = modulo (resta c i)
 
 argumento :: Complejo -> Float
-argumento c | atan ((snd c)/(fst c)) >= 0 = atan ((snd c)/(fst c))
-            | otherwise = atan ((snd c)/(fst c)) + 2*pi
+argumento c | c == (0,0) = 0
+            | fst c == 0 && snd c > 0 = pi/2
+            | fst c == 0 && snd c < 0 = 3*pi/2
+            | snd c == 0 && fst c > 0 = 0
+            | snd c == 0 && fst c < 0 = pi
+            | fst c > 0 = atan ((snd c)/(fst c))
+            | fst c < 0 && snd c < 0 = atan ((snd c)/(fst c)) - pi
+            | fst c < 0 && snd c > 0 = atan ((snd c)/(fst c)) + pi
+
 
 pasarACartesianas :: Float -> Float -> Complejo
 pasarACartesianas r o = (r*cos(o),r*sin(o))
+
+raizCuadrada :: Complejo -> (Complejo,Complejo)
+raizCuadrada c = (pasarACartesianas ((modulo c)**(1/2)) (argumento(c)/2), pasarACartesianas ((modulo c)**(1/2)) ((argumento(c)+2*pi)/2))
+
+raicesCuadraticaCompleja :: Complejo -> Complejo -> Complejo -> (Complejo,Complejo)
+raicesCuadraticaCompleja a b c = (cociente (suma (producto b (-1,0)) (fst (raizCuadrada (discriminante a b c)))) (producto (2,0) a), 
+                                  cociente (suma (producto b (-1,0)) (snd (raizCuadrada (discriminante a b c)))) (producto (2,0) a))
+
+discriminante :: Complejo -> Complejo -> Complejo -> Complejo
+discriminante a b c =  suma (potencia b 2) (producto (producto a c) (-4,0))
+
+raicesNEsimasAux :: Float -> Float -> [Complejo]
+raicesNEsimasAux n k | k==0 = [(pasarACartesianas 1 0)]
+                     | otherwise = (pasarACartesianas 1 ((2*k*pi)/n)) : (raicesNEsimasAux n (k-1))
+
+raicesNEsimas :: Integer -> [Complejo]
+raicesNEsimas n = raicesNEsimasAux (fromIntegral n) (fromIntegral(n-1))
+
+--sonRaicesNEsimas :: Integer -> [Complejo] -> Float -> Bool
